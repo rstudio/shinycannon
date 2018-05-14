0e9153a30eef7811a66278b73396d969bd360334
Alan Dipert <alan@dipert.org>
2018-05-14 16:02:42 -0700

    WIP obtaining login cookie

--- src/main/kotlin/com/rstudio/shinycannon/App.kt
+++ src/main/kotlin/com/rstudio/shinycannon/App.kt
@@ -1,27 +1,48 @@
 package com.rstudio.shinycannon
 
+import net.moznion.uribuildertiny.URIBuilderTiny
+import org.apache.http.client.config.CookieSpecs
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.client.entity.UrlEncodedFormEntity
 import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpPost
 import org.apache.http.client.methods.HttpUriRequest
+import org.apache.http.impl.client.BasicCookieStore
 import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.message.BasicNameValuePair
+import org.w3c.dom.NamedNodeMap
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
 import org.xml.sax.InputSource
 import java.io.ByteArrayOutputStream
 import java.io.StringReader
+import java.net.URL
+import java.util.ArrayList
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.xpath.XPathConstants
 import javax.xml.xpath.XPathFactory
 
 data class HttpResponse(val statusCode: Int,
                         val headers: Map<String, String>,
+                        val cookies: BasicCookieStore,
                         val body: String)
 
 fun slurp(req: HttpUriRequest): HttpResponse {
-    HttpClientBuilder.create().build().execute(req).use { response ->
+    val cookies = BasicCookieStore()
+    val cfg = RequestConfig.custom()
+            .setCookieSpec(CookieSpecs.STANDARD)
+            .build()
+    val client = HttpClientBuilder
+            .create()
+            .setDefaultCookieStore(cookies)
+            .setDefaultRequestConfig(cfg)
+            .build()
+    client.execute(req).use { response ->
         return HttpResponse(response.statusLine.statusCode,
                 response.allHeaders.fold(mapOf(), { m, h ->
                     m + Pair(h.name, h.value)
                 }),
+                cookies,
                 ByteArrayOutputStream().let { baos ->
                     response.entity.content.copyTo(baos)
                     baos.toString()
@@ -39,45 +60,86 @@ fun xpath(docString: String, query: String): Array<Node> {
     return Array(nodes.length, { i -> nodes.item(i) })
 }
 
+// Helper function (overrides []) for getting keys from a NamedNodeList, a weird
+// XML collection type produced by the XPath API.
+operator fun NamedNodeMap.get(itemName: String): String = this.getNamedItem(itemName).nodeValue
+
 enum class AppServer { RSC, SSP, UNKNOWN }
 
-class App(val appUrl: String) {
-    companion object {
-        fun isProtected(appUrl: String): Boolean {
-            return setOf(403, 404).contains(slurp(HttpGet(appUrl)).statusCode)
-        }
+fun isProtected(appUrl: String): Boolean {
+    return setOf(403, 404).contains(slurp(HttpGet(appUrl)).statusCode)
+}
 
-        fun servedBy(resp: HttpResponse): AppServer {
-            val headers = resp.headers
-            return when {
-                // TODO figure out why SSP-XSRF not served by 1.5.8.960
-                headers["X-Powered-By"] == "Express" -> AppServer.SSP
-                headers.containsKey("rscid") -> AppServer.RSC
-                else -> AppServer.UNKNOWN
-            }
-        }
-
-        fun getInputs(resp: HttpResponse, server: AppServer): Map<String, String> {
-            return when (server) {
-                AppServer.SSP -> {
-                    xpath(resp.body, "//input[@type='hidden']").fold(
-                            mapOf(),
-                            { xs, y ->
-                                xs + listOf("name", "value")
-                                        .map { y.attributes.getNamedItem(it).nodeValue }
-                                        .zipWithNext()
-                                        .first()
-                            }
-                    )
-                }
-                else -> mapOf()
-            }
-        }
+fun servedBy(resp: HttpResponse): AppServer {
+    val headers = resp.headers
+    return when {
+    // TODO figure out why SSP-XSRF not served by 1.5.8.960
+        headers["X-Powered-By"] == "Express" -> AppServer.SSP
+        headers.containsKey("rscid") -> AppServer.RSC
+        else -> AppServer.UNKNOWN
     }
+}
 
+// Returns a Map of hidden inputs that must be posted along with
+// username and password. The map is empty except for SSP.
+fun getInputs(resp: HttpResponse, server: AppServer): Map<String, String> {
+    return when (server) {
+        AppServer.SSP -> {
+            xpath(resp.body, "//input[@type='hidden']")
+                    .map { it.attributes }
+                    .map { attrs -> Pair(attrs["name"], attrs["value"]) }
+                    .fold(mapOf(), { m, pair -> m + pair })
+        }
+        else -> mapOf()
+    }
+}
+
+fun loginUrlFor(appUrl: String, server: AppServer): String {
+    return when (server) {
+        AppServer.RSC -> URL(appUrl).let { url ->
+            URIBuilderTiny()
+                    .setScheme(url.protocol)
+                    .setHost(url.host)
+                    .setPort(url.port)
+                    .appendPaths("__login__")
+                    .build()
+                    .toString()
+        }
+        AppServer.SSP -> URIBuilderTiny(appUrl)
+                .appendPaths("__login__")
+                .build()
+                .toString()
+        else -> error("Don't know how to construct login URL for ${server}")
+    }
+}
+
+class App(val appUrl: String) {
     private val resp = slurp(HttpGet(appUrl))
     val server = servedBy(resp)
     val inputs = getInputs(resp, server)
+    val loginUrl = loginUrlFor(appUrl, server)
+
+    fun postLogin(username: String, password: String) {
+        val cfg = RequestConfig.custom()
+                .setCookieSpec(CookieSpecs.STANDARD)
+                .build()
+        val client = HttpClientBuilder
+                .create()
+                .setDefaultCookieStore(resp.cookies)
+                .setDefaultRequestConfig(cfg)
+                .build()
+        val post = HttpPost(loginUrl)
+        val formFields = mapOf(
+                "username" to username,
+                "password" to password
+        ) + inputs
+        post.entity = formFields.map { entry ->
+            BasicNameValuePair(entry.key, entry.value)
+        }.let { pairs -> UrlEncodedFormEntity(pairs) }
+        client.execute(post).use { response ->
+        }
+
+    }
 }
 
 fun main(args: Array<String>) {
