e75159209b6ad0b20ddbab3f1c74caa25c7b21c3
Alan Dipert <alan@dipert.org>
2018-07-09 12:13:20 -0700

    --workers instead of --sessions, better docs, --loaded-duration-minutes can be fractional

--- src/main/kotlin/com/rstudio/shinycannon/Main.kt
+++ src/main/kotlin/com/rstudio/shinycannon/Main.kt
@@ -18,6 +18,7 @@ import org.apache.log4j.PatternLayout
 import java.io.File
 import java.io.PrintWriter
 import java.lang.Exception
+import java.math.BigDecimal
 import java.nio.file.Paths
 import java.security.SecureRandom
 import java.time.Instant
@@ -219,12 +220,12 @@ fun info(msg: String) {
 class EnduranceTest(val args: Sequence<String>,
                     val httpUrl: String,
                     val logPath: String,
-                    // Amount of time to wait between starting sessions until target reached
+                    // Amount of time to wait between starting workers until target reached
                     val warmupInterval: Int = 0,
-                    // Time to maintain target number of sessions
-                    val loadedDurationMinutes: Int,
-                    // Number of sessions to maintain
-                    val numSessions: Int,
+                    // Time to maintain target number of workers
+                    val loadedDurationMinutes: BigDecimal,
+                    // Number of workers to maintain
+                    val numWorkers: Int,
                     val outputDir: File) {
 
     val columnNames = arrayOf("session_id", "worker_id", "iteration", "event", "timestamp", "input_line_number", "comment")
@@ -244,7 +245,7 @@ class EnduranceTest(val args: Sequence<String>,
 
         fun makeOutputFile(sessionId: Int, workerId: Int, iterationId: Int) = outputDir
                 .toPath()
-                .resolve(Paths.get("sessions", "${sessionId}_${workerId}_${iterationId}.log"))
+                .resolve(Paths.get("workers", "${sessionId}_${workerId}_${iterationId}.csv"))
                 .toFile()
 
         fun startSession(sessionId: Int, workerId: Int, iterationId: Int, delay: Int = 0) {
@@ -267,10 +268,10 @@ class EnduranceTest(val args: Sequence<String>,
             println("${Instant.now().toString()} - $stats")
         }
 
-        val warmupCountdown = CountDownLatch(numSessions)
-        val finishedCountdown = CountDownLatch(numSessions)
+        val warmupCountdown = CountDownLatch(numWorkers)
+        val finishedCountdown = CountDownLatch(numWorkers)
 
-        for (worker in 0 until numSessions) {
+        for (worker in 0 until numWorkers) {
             thread {
                 var iteration = 0
                 // Continue after some (possibly-zero) millisecond delay
@@ -279,7 +280,7 @@ class EnduranceTest(val args: Sequence<String>,
                 warmupCountdown.countDown()
                 startSession(sessionNum.getAndIncrement(), worker, iteration++)
                 while (keepWorking.get()) {
-                    // Subsequent sessions start immediately
+                    // Subsequent workers start immediately
                     info("Worker $worker running again")
                     startSession(sessionNum.getAndIncrement(), worker, iteration++)
                 }
@@ -291,8 +292,8 @@ class EnduranceTest(val args: Sequence<String>,
         info("Waiting for warmup to complete")
         warmupCountdown.await()
         // TODO minutes should be able to be fractional
-        info("Maintaining for $loadedDurationMinutes minutes")
-        Thread.sleep(loadedDurationMinutes*60*1000.toLong())
+        info("Maintaining for $loadedDurationMinutes minutes (${loadedDurationMinutes*BigDecimal(60000)} ms)")
+        Thread.sleep((loadedDurationMinutes*BigDecimal(60000)).toLong())
         info("Stopped maintaining, waiting for workers to stop")
         keepWorking.set(false)
         finishedCountdown.await()
@@ -303,16 +304,16 @@ class EnduranceTest(val args: Sequence<String>,
 }
 
 class Args(parser: ArgParser) {
-    val logPath by parser.positional("Path to Shiny interaction log file")
+    val recordingPath by parser.positional("Path to recording file")
     val appUrl by parser.positional("URL of the Shiny application to interact with")
-    val sessions by parser.storing("Number of sessions to simulate. Default is 1.") { toInt() }
+    val workers by parser.storing("Number of workers to simulate. Default is 1.") { toInt() }
             .default(1)
-    val loadedDurationMinutes by parser.storing("Number of minutes to maintain load after all sessions have started.") { toInt() }
-            .default(0)
+    val loadedDurationMinutes by parser.storing("Number of minutes to continue simulating sessions in each worker after all workers have completed one session. Can be fractional. Default is 0.") { toBigDecimal() }
+            .default(BigDecimal.ZERO)
     val outputDir by parser.storing("Path to directory to store session logs in for this test run")
             .default("test-logs-${Instant.now()}")
     val overwriteOutput by parser.flagging("Whether or not to delete the output directory before starting, if it exists already")
-    val startInterval by parser.storing("Number of milliseconds to wait between starting sessions") { toInt() }
+    val startInterval by parser.storing("Number of milliseconds to wait between starting workers") { toInt() }
             .default(0)
     val logLevel by parser.storing("Log level (default: warn, available include: debug, info, warn, error)") {
         Level.toLevel(this.toUpperCase(), Level.WARN) as Level
@@ -331,7 +332,12 @@ fun main(args: Array<String>) = mainBody("shinycannon") {
         val output = File(outputDir)
         if (output.exists()) {
             if (overwriteOutput) {
-                // TODO Ensure the existing directory we're about to delete is conceivably an output directory.
+                // Ensure the existing directory we're about to delete is conceivably an output directory.
+                check(listOf("recording.log", "detail.log", "sessions").map {
+                    output.toPath().resolve(it).toFile()
+                }.all { it.exists() }, {
+                    "Directory doesn't look like an output directory, so not overwriting. Please delete it manually."
+                })
                 output.deleteRecursively()
             } else {
                 error("Output dir $outputDir already exists and --overwrite-output not set")
@@ -339,7 +345,7 @@ fun main(args: Array<String>) = mainBody("shinycannon") {
         }
 
         output.mkdirs()
-        output.toPath().resolve("sessions").toFile().mkdir()
+        output.toPath().resolve("workers").toFile().mkdir()
 
         // TODO Default to warn, and don't make a special detail.log: print thees messages to the console.
         val fa = FileAppender()
@@ -360,14 +366,14 @@ fun main(args: Array<String>) = mainBody("shinycannon") {
         }
 
         // Copy the recording file to the output directory so runs are easily reproducible.
-        File(logPath).copyTo(output.toPath().resolve("recording.log").toFile())
+        File(recordingPath).copyTo(output.toPath().resolve("recording.log").toFile())
 
         val loadTest = EnduranceTest(
                 // Drop the original logpath from the arglist
                 args.asSequence().drop(1),
                 appUrl,
-                logPath,
-                numSessions = sessions,
+                recordingPath,
+                numWorkers = workers,
                 outputDir = output,
                 warmupInterval = startInterval,
                 loadedDurationMinutes = loadedDurationMinutes
