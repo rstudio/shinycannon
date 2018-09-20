f2cd5202143b412c341950a6865b560aa671dff1
Alan Dipert <alan@dipert.org>
2018-09-20 16:06:53 -0700

    Display error message on Linux/Mac if java not found.
    - refs #16

--- head.sh
+++ head.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+if [[ ! $(which java) ]]; then
+>&2 cat << EOF
+java command not found.
+Please see https://rstudio.github.io/shinyloadtest/
+for installation instructions.
+EOF
+exit 1
+fi
 exec java -jar "$0" "$@"
